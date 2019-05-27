4dbf55d23e4b306a2392e5de2534a649140986a5
Satu Untuk Semua <satu.untuk.semua.aja@gmail.com>
2019-05-27 23:28:31 +0800

    404,403,500

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -10,11 +10,11 @@
 </style>
 <body background="">    
     <img src="images/Logo.png" style="max-height:50%;max-width:50%; margin-left:25%;">
-    <p style="font-size:100px;color:white;margin-left:30%">404 Not Found</p>
-    <p style="font-size:25px;color:white;margin-left:24%">Oops.. Like you got lost, Please check your URL or Click the "Home" Button</p>
+    <p style="font-size:100px;color:white;margin-left:27%">403 Forbidden!!</p>
+    <p style="font-size:25px;color:white;margin-left:26%">Oops.. you can't access this page, Please Click the "Home" Button</p>
     <form action="/">
         <div class="col-md-12">
-            <input type="submit" id="login_update_button" style="background-color: transparent;margin-left:47%" class="btn btn-default pull-left" value="Home">
+            <input type="submit" id="login_update_button" style="background-color: transparent;margin-left:45%" class="btn btn-default pull-left" value="Home">
         </div>
     </form>
 </body>
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -10,11 +10,11 @@
 </style>
 <body background="">    
     <img src="images/Logo.png" style="max-height:50%;max-width:50%; margin-left:25%;">
-    <p style="font-size:100px;color:white;margin-left:30%">404 Not Found</p>
-    <p style="font-size:25px;color:white;margin-left:24%">Oops.. Like you got lost, Please check your URL or Click the "Home" Button</p>
+    <p style="font-size:100px;color:white;margin-left:23.5%">500 Server Errors</p>
+    <p style="font-size:25px;color:white;margin-left:20%">Oops.. it's seems something wrong here, Please Click the "Home" Button and try again</p>
     <form action="/">
         <div class="col-md-12">
-            <input type="submit" id="login_update_button" style="background-color: transparent;margin-left:47%" class="btn btn-default pull-left" value="Home">
+            <input type="submit" id="login_update_button" style="background-color: transparent;margin-left:45%" class="btn btn-default pull-left" value="Home">
         </div>
     </form>
 </body>

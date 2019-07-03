edce43e1e46857ccdb20041de69876ced2d30b20
Satu Untuk Semua <satu.untuk.semua.aja@gmail.com>
2019-07-04 03:04:46 +0800

    video phone

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 <div id="site-content">
     <div class="_5m2ieb" style="background-image:url(../images/Background/InsiderSuite_Home1.jpg)">
 
-        <video loop="" muted="" playsinline="" autoplay="" style="object-fit:cover;width:100%;height:100%">
+        <video id="video" loop="" muted="" playsinline="" autoplay="" style="object-fit:cover;width:100%;height:100%">
           <source src="https://dddwzx8rabh1g.cloudfront.net/home_video.mp4">
           <source src="https://dddwzx8rabh1g.cloudfront.net/home_video.mp4">
     </video>
@@ -17,181 +17,57 @@
                 <div>
                     <section>
                         <div class="_1hargc54">
-                            <h1 tabindex="-1" class="_tpbrp">Upgrade your holiday... 
-Your way...</h1>
+                            <h1 tabindex="-1" class="_tpbrp">Pimp your trip</h1>
                         </div>
                         <div class="_byeukid">
-                              What could be better than travelling with the Spirit of Freedom? More travelling, less worrying!
-                                    <br> Insider Suite will spare you the hassle of preparing your vacay tailored to your dreams.
-                               <br></div>
+                            Design your entire holiday with supreme accommodation and breathtaking activities in the most hyped-up locations.</br> Secret offers of up to 80% off are at your fingertips!
+                        </div>
                     </section>
                 </div>
             </div>
             <div>
                 <div>
                     <div class="_36rlri" style="margin-bottom:8px;margin-right:8px">
-                        <a href=" href=" #"="" data-toggle="modal" data-target="#authentication" "="" class="_fcgkkz8">Access the offers</a>
+                        <a href="@if(Auth::User()) {{ url('offers') }} @else href="#" data-toggle="modal" data-target="#authentication" @endif" class="_fcgkkz8">Access the offers</a>
                     </div>
-                    <a href="https://www.insidersuite.com/our_story" class="_1tesales" aria-busy="false"><span class="_cgr7tc7">Our Story</span></a>
+                    <a href="#how-are-exp-different" class="_1tesales" aria-busy="false"><span class="_cgr7tc7">Our Story</span></a>
                 </div>
             </div>
         </div>
 
     </div>
-       <div class="text-content _1q61p8r">
-        <div class="solid-content">
-            <div class="_113qkde">
-                <div class="_n3xqk0l"><h1 tabindex="-1" class="_tpbrp">Join a group trip</h1></div>
-                <div class="_1bvsgufu">We believe that not having friends to travel with is enough for some to give up their travel dreams. Insider Suite Group invites you to travel with like-minded people that love adventure and explore new destinations in an unconventional &amp; hassle-free way.
-
-    
-    Meet new people, share “unboring” experience and unlock your potential from the moment you land to the moment you leave. 
-</div>
-
-<div class="subscribe">
-                              <a href="https://www.insidersuite-group.com" target="_blank" class="btn btn-subscribe">Join now</a>
+     <div class="text-content _1q61p8r"">
+        <div id="how-are-exp-different">
+                <div class="solid-content_full" >
+                    <div class="_hjkdye">
+                        <section>
+                            <div class="_n3xqk0l">
+                                <h1 tabindex="-1" class="_tpbrpy">Upgrade your holiday... <br/> Your way...</h1>
+                            </div>
+                            <div style="margin-top:0">
+                                <div class="_1bvsgufuy">
+                                    What could be better than travelling with the Spirit of Freedom? More smiling, less worrying!
+                                    <b>Insider Suite </b>will spare you the hassle of preparing your vacay by handpicking the most refined accommodation and activities tailored to your dreams.
+                                    Enjoy secret and exclusive offers, specially negotiated and packaged for the unconventional traveller like you. It’s time to become an Insider Suite crew member!
+                                </div>
+                            </div>
+                            <div class="subscribe">
+                              <a href="@if(Auth::User()) {{ url('offers') }} @else href="#" data-toggle="modal" data-target="#authentication" @endif" class="btn btn-subscribe">Join Now</a>
                              </div>
-            </div>
-        </div>
-        <div class="_1xyzu4b">
-            <div class="_89pa8d">
-                <ul class="rslides">
-                    <li>
-                        <div class="_fklum6">
-                            <div class="_ximq7y">
-                                <div class="_zp9vmh">
-                                    <div class="_103cimmg" role="button" tabindex="0">
-                                        <div class="media-card-image _8ry8sn _1hdg0i6" style="background:#1a1005 url(../images/Background/group_trip.jpg) center;/* object-fit: cover; */"></div>
-                                        <div class="_1mwbqe72" style="background:linear-gradient(0deg, #1a1005 0%, rgba(0,0,0,0) 100%)"></div>
-                                        
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="_fklum6">
-                            <div class="_ximq7y">
-                                <div class="_zp9vmh">
-                                    <div class="_103cimmg" role="button" tabindex="0">
-                                        <div class="media-card-image _8ry8sn _1hdg0i6" style="background:#031514 url(https://a0.muscache.com/airbnb/static/launch_platform/experience-hosting-v2/CarouselMediaCard_1_2@2x-6efd085e2f4f7cfcadea2eae7cee1f9e.jpg) center"></div>
-                                        <div class="_1mwbqe72" style="background:linear-gradient(0deg, #031514 0%, rgba(0,0,0,0) 100%)"></div>
-                                        <div class="_fmiriw">
-                                            <div class="_1459jxf">
-                                                <div class="_1rhw8m2q">“I once hosted a woman who had never seen or been in the ocean. It was amazing to see her splashing around in the water!”</div>
-                                            </div>
-                                            <div class="_1rhw8m2q">João</div>
-                                            <div class="_s1d1uad">Hosts The Natural Adventure in Lisbon</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="_fklum6">
-                            <div class="_ximq7y">
-                                <div class="_zp9vmh">
-                                    <div class="_103cimmg" role="button" tabindex="0">
-                                        <div class="media-card-image _8ry8sn _1hdg0i6" style="background:#150a03 url(https://a0.muscache.com/airbnb/static/launch_platform/experience-hosting-v2/CarouselMediaCard_1_3@2x-f517dc207ed89b769aacafc450107c6d.jpg) center"></div>
-                                        <div class="_1mwbqe72" style="background:linear-gradient(0deg, #150a03 0%, rgba(0,0,0,0) 100%)"></div>
-                                        <div class="_fmiriw">
-                                            <div class="_1459jxf">
-                                                <div class="_1rhw8m2q">“I took guests to hear classical music by an obscure composer and they loved it! It shows that people can be open to new experiences.”</div>
-                                            </div>
-                                            <div class="_1rhw8m2q">Ben</div>
-                                            <div class="_s1d1uad">Hosts A Classical Wander in London</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <div class="text-content _1q61p8r">
-        <div class="_1xyzu4b">
-            <div class="_89pa8d">
-                <ul class="rslides">
-                    <li>
-                        <div class="_fklum6">
-                            <div class="_ximq7y">
-                                <div class="_zp9vmh">
-                                    <div class="_103cimmg" role="button" tabindex="0">
-                                        <div class="media-card-image _8ry8sn _1hdg0i6" style="background:#150d04 url(../images/Background/own_trip.jpg) center"></div>
-                                        <div class="_1mwbqe72" style="background:linear-gradient(0deg, #1a1005 0%, rgba(0,0,0,0) 100%)"></div>
-                                        
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="_fklum6">
-                            <div class="_ximq7y">
-                                <div class="_zp9vmh">
-                                    <div class="_103cimmg" role="button" tabindex="0">
-                                        <div class="media-card-image _8ry8sn _1hdg0i6" style="background:#03080e url(https://a0.muscache.com/airbnb/static/launch_platform/experience-hosting-v2/CarouselMediaCard_2_3@2x-154344e8552c17b7ac5e26a98b56d97e.jpg) center"></div>
-                                        <div class="_1mwbqe72" style="background:linear-gradient(0deg, #031514 0%, rgba(0,0,0,0) 100%)"></div>
-                                        <div class="_fmiriw">
-                                            <span class="_1birhh0"><svg viewBox="0 0 1000 1000" role="presentation" aria-hidden="true" focusable="false" style="height:16px;width:16px;display:block;fill:#ffffff"><path d="M971.5 379.5c9 28 2 50-20 67L725.4 618.6l87 280.1c11 39-18 75-54 75-12 0-23-4-33-12l-226.1-172-226.1 172.1c-25 17-59 12-78-12-12-16-15-33-8-51l86-278.1L46.1 446.5c-21-17-28-39-19-67 8-24 29-40 52-40h280.1l87-278.1c7-23 28-39 52-39 25 0 47 17 54 41l87 276.1h280.1c23.2 0 44.2 16 52.2 40z"></path></svg></span>
-                                            <span class="_1birhh0"><svg viewBox="0 0 1000 1000" role="presentation" aria-hidden="true" focusable="false" style="height:16px;width:16px;display:block;fill:#ffffff"><path d="M971.5 379.5c9 28 2 50-20 67L725.4 618.6l87 280.1c11 39-18 75-54 75-12 0-23-4-33-12l-226.1-172-226.1 172.1c-25 17-59 12-78-12-12-16-15-33-8-51l86-278.1L46.1 446.5c-21-17-28-39-19-67 8-24 29-40 52-40h280.1l87-278.1c7-23 28-39 52-39 25 0 47 17 54 41l87 276.1h280.1c23.2 0 44.2 16 52.2 40z"></path></svg></span>
-                                            <span class="_1birhh0"><svg viewBox="0 0 1000 1000" role="presentation" aria-hidden="true" focusable="false" style="height:16px;width:16px;display:block;fill:#ffffff"><path d="M971.5 379.5c9 28 2 50-20 67L725.4 618.6l87 280.1c11 39-18 75-54 75-12 0-23-4-33-12l-226.1-172-226.1 172.1c-25 17-59 12-78-12-12-16-15-33-8-51l86-278.1L46.1 446.5c-21-17-28-39-19-67 8-24 29-40 52-40h280.1l87-278.1c7-23 28-39 52-39 25 0 47 17 54 41l87 276.1h280.1c23.2 0 44.2 16 52.2 40z"></path></svg></span>
-                                            <span class="_1birhh0"><svg viewBox="0 0 1000 1000" role="presentation" aria-hidden="true" focusable="false" style="height:16px;width:16px;display:block;fill:#ffffff"><path d="M971.5 379.5c9 28 2 50-20 67L725.4 618.6l87 280.1c11 39-18 75-54 75-12 0-23-4-33-12l-226.1-172-226.1 172.1c-25 17-59 12-78-12-12-16-15-33-8-51l86-278.1L46.1 446.5c-21-17-28-39-19-67 8-24 29-40 52-40h280.1l87-278.1c7-23 28-39 52-39 25 0 47 17 54 41l87 276.1h280.1c23.2 0 44.2 16 52.2 40z"></path></svg></span>
-                                            <span class="_1birhh0"><svg viewBox="0 0 1000 1000" role="presentation" aria-hidden="true" focusable="false" style="height:16px;width:16px;display:block;fill:#ffffff"><path d="M971.5 379.5c9 28 2 50-20 67L725.4 618.6l87 280.1c11 39-18 75-54 75-12 0-23-4-33-12l-226.1-172-226.1 172.1c-25 17-59 12-78-12-12-16-15-33-8-51l86-278.1L46.1 446.5c-21-17-28-39-19-67 8-24 29-40 52-40h280.1l87-278.1c7-23 28-39 52-39 25 0 47 17 54 41l87 276.1h280.1c23.2 0 44.2 16 52.2 40z"></path></svg></span>
-                                            <div class="_1459jxf">
-                                                <div class="_1rhw8m2q">“Running with Gelin was one of my favorite memories from Cuba. He’s fun and very knowledgeable about history.”</div>
-                                            </div>
-                                            <div class="_1rhw8m2q">Simona</div>
-                                            <div class="_s1d1uad">Guest on Culture Run in Havana</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="_fklum6">
-                            <div class="_ximq7y">
-                                <div class="_zp9vmh">
-                                    <div class="_103cimmg" role="button" tabindex="0">
-                                        <div class="media-card-image _8ry8sn _1hdg0i6" style="background:#050a16 url(https://a0.muscache.com/airbnb/static/launch_platform/experience-hosting-v2/CarouselMediaCard_2_1@2x-e132b159ac7ba7ff9d551aad029c215b.jpg) center"></div>
-                                        <div class="_1mwbqe72" style="background:linear-gradient(0deg, #150a03 0%, rgba(0,0,0,0) 100%)"></div>
-                                        <div class="_fmiriw">
-                                            <span class="_1birhh0"><svg viewBox="0 0 1000 1000" role="presentation" aria-hidden="true" focusable="false" style="height:16px;width:16px;display:block;fill:#ffffff"><path d="M971.5 379.5c9 28 2 50-20 67L725.4 618.6l87 280.1c11 39-18 75-54 75-12 0-23-4-33-12l-226.1-172-226.1 172.1c-25 17-59 12-78-12-12-16-15-33-8-51l86-278.1L46.1 446.5c-21-17-28-39-19-67 8-24 29-40 52-40h280.1l87-278.1c7-23 28-39 52-39 25 0 47 17 54 41l87 276.1h280.1c23.2 0 44.2 16 52.2 40z"></path></svg></span>
-                                            <span class="_1birhh0"><svg viewBox="0 0 1000 1000" role="presentation" aria-hidden="true" focusable="false" style="height:16px;width:16px;display:block;fill:#ffffff"><path d="M971.5 379.5c9 28 2 50-20 67L725.4 618.6l87 280.1c11 39-18 75-54 75-12 0-23-4-33-12l-226.1-172-226.1 172.1c-25 17-59 12-78-12-12-16-15-33-8-51l86-278.1L46.1 446.5c-21-17-28-39-19-67 8-24 29-40 52-40h280.1l87-278.1c7-23 28-39 52-39 25 0 47 17 54 41l87 276.1h280.1c23.2 0 44.2 16 52.2 40z"></path></svg></span>
-                                            <span class="_1birhh0"><svg viewBox="0 0 1000 1000" role="presentation" aria-hidden="true" focusable="false" style="height:16px;width:16px;display:block;fill:#ffffff"><path d="M971.5 379.5c9 28 2 50-20 67L725.4 618.6l87 280.1c11 39-18 75-54 75-12 0-23-4-33-12l-226.1-172-226.1 172.1c-25 17-59 12-78-12-12-16-15-33-8-51l86-278.1L46.1 446.5c-21-17-28-39-19-67 8-24 29-40 52-40h280.1l87-278.1c7-23 28-39 52-39 25 0 47 17 54 41l87 276.1h280.1c23.2 0 44.2 16 52.2 40z"></path></svg></span>
-                                            <span class="_1birhh0"><svg viewBox="0 0 1000 1000" role="presentation" aria-hidden="true" focusable="false" style="height:16px;width:16px;display:block;fill:#ffffff"><path d="M971.5 379.5c9 28 2 50-20 67L725.4 618.6l87 280.1c11 39-18 75-54 75-12 0-23-4-33-12l-226.1-172-226.1 172.1c-25 17-59 12-78-12-12-16-15-33-8-51l86-278.1L46.1 446.5c-21-17-28-39-19-67 8-24 29-40 52-40h280.1l87-278.1c7-23 28-39 52-39 25 0 47 17 54 41l87 276.1h280.1c23.2 0 44.2 16 52.2 40z"></path></svg></span>
-                                            <span class="_1birhh0"><svg viewBox="0 0 1000 1000" role="presentation" aria-hidden="true" focusable="false" style="height:16px;width:16px;display:block;fill:#ffffff"><path d="M971.5 379.5c9 28 2 50-20 67L725.4 618.6l87 280.1c11 39-18 75-54 75-12 0-23-4-33-12l-226.1-172-226.1 172.1c-25 17-59 12-78-12-12-16-15-33-8-51l86-278.1L46.1 446.5c-21-17-28-39-19-67 8-24 29-40 52-40h280.1l87-278.1c7-23 28-39 52-39 25 0 47 17 54 41l87 276.1h280.1c23.2 0 44.2 16 52.2 40z"></path></svg></span>
-                                            <div class="_1459jxf">
-                                                <div class="_1rhw8m2q">“Caitlin’s energy was infectious. She was clearly both knowledgeable and passionate about foraging.”</div>
-                                            </div>
-                                            <div class="_1rhw8m2q">Tim</div>
-                                            <div class="_s1d1uad">Guest on Forage Edible Plants in Santa Cruz</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="solid-content added-style">
-            <div class="_113qkde">
-                <div class="_n3xqk0l"><h1 tabindex="-1" class="_tpbrp">Build your own trip</h1></div>
-                <div class="_1bvsgufu">Design your entire holiday with supreme accommodation and breathtaking activities in the most hyped-up locations.
-Secret offers of up to 80% off are at your fingertips!<b></b></div>
-
-<div class="subscribe">
-                              <a href=" href=" #"="" data-toggle="modal" data-target="#authentication" "="" class="btn btn-subscribe">Join now</a>
-                             </div>
+                        </section>
+                    </div>
             </div>
         </div>
     </div>
     
 </div>
+@endsection
+@section('scripts')
+    <script>
+        var video = document.getElementById('video'); 
+        video.addEventListener('click',function(){ 
+            video.play(); 
+        },false);
+    </script>
 @endsection
 
